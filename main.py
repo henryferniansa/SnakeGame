0a34a8f12fa7444dbdfcd0af53e47af15f91fd29
henryferniansa <110883850+henryferniansa@users.noreply.github.com>
2025-02-03 05:53:46 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import time
+from turtle import Screen
+from food import Food
+from scoreboard import Score
+
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.scoring()
+        snake.extend()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        score.game_over()
+
+    for segment in snake.segment:
+        if segment == snake.head:
+            pass
+        if snake.head.distance() < 10:
+            game_is_on = False
+            score.game_over()
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update()
+        self.hideturtle()
+
+    def update(self):
+        self.write(f"Score : {self.score}",move=False ,align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write(f"Game Over",move=False ,align=ALIGNMENT, font=FONT)
+
+
+    def scoring(self):
+        self.score += 1
+        self.clear()
+        self.update()
